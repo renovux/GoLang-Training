132c61ae98555949c264f4cf3f161376bff2f3a2
Nicholas Ciachin <r3novas@gmail.com>
2017-10-21 09:17:36 -0400

    Full Commit

--- 03_variables/03_less-emphasis/08_exercise_your-name/01_oneSolution/myNameVar.go
+++ 03_variables/03_less-emphasis/08_exercise_your-name/01_oneSolution/myNameVar.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-var name = "Todd"
+var name = "Nicholas"
 
 func main() {
 	fmt.Println("Hello ", name)
--- 03_variables/03_less-emphasis/08_exercise_your-name/02_anotherSolution/myNameVar.go
+++ 03_variables/03_less-emphasis/08_exercise_your-name/02_anotherSolution/myNameVar.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	var name = "Todd"
+	var name = "Nicholas"
 	fmt.Println("Hello ", name)
 }
