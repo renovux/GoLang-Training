b8661ae43bc4351d039f82d9ccf1e6b72467b9ed
Nicholas Ciachin <r3novas@gmail.com>
2017-10-21 08:48:13 -0400

    Full Commit

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/renovux/udemy-training/02_package/icomefromnewyork"
 	"github.com/renovux/udemy-training/02_package/stringutil"
-	//someAlias "github.com/renovux/udemy-training/02_package/icomefromalaska"
+	//someAlias "github.com/renovux/udemy-training/02_package/icomefromnewyork"
 )
 
 func main() {
